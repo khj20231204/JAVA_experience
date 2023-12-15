2704ac32fd0be40b44839cd9b46bcb96ec08a667
khj2023 <natista00@naver.com>
2023-12-15 18:03:40 +0900

    first commit

---
+++ src/Ex_list.java
@@ -0,0 +1,5 @@
+public class Ex_list {
+    void explain(){
+        System.out.println("list explain");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        Ex_list el = new Ex_list();
+        el.explain();
+    }
+}
