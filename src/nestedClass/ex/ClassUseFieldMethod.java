6dcc100e0707d535a6d6a71376c5180bf0a88bce
khj2023 <natista00@naver.com>
2024-01-01 21:01:03 +0900

    nestedClass

---
+++ src/nestedClass/ex/ClassUseFieldMethod.java
@@ -0,0 +1,30 @@
+package nestedClass.ex;
+
+public class ClassUseFieldMethod {
+    int myfield;
+    static int staticFiled;
+
+    public void instanceMethod(){}
+
+    public static void staticMethod(){}
+
+    class instanceClass{
+        void method(){
+            myfield = 10;
+            staticFiled = 11;
+
+            instanceMethod();
+            staticMethod();
+        }
+    }
+
+    static class MyStaticClass{
+        void method(){
+            //myfield = 10;  error
+            staticFiled = 11;
+
+            //instanceMethod();  error
+            staticMethod();
+        }
+    }
+}
---
+++ src/nestedClass/ex/FieldMethodUseClass.java
@@ -0,0 +1,20 @@
+package nestedClass.ex;
+
+public class FieldMethodUseClass {
+    //필드나 메소드에서 클래스를 사용시 주의 점
+    class InstanceClass{}
+    static class MyStaticClass{ }
+
+    InstanceClass uc = new InstanceClass();
+    MyStaticClass sc = new MyStaticClass();
+
+    void instanceMethod(){
+        InstanceClass uc = new InstanceClass();
+        MyStaticClass sc = new MyStaticClass();
+    }
+
+    static void staticMethod(){
+        //InstanceClass uc = new InstanceClass();  error
+        MyStaticClass sc = new MyStaticClass();
+    }
+}
