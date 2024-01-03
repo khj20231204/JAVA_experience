a73098abab06ef9cb3905feef56885e97a687b88
khj2023 <natista00@naver.com>
2024-01-03 21:18:20 +0900

    Iterator.ex

---
+++ src/collectionframework/ex/Main.java
@@ -0,0 +1,51 @@
+package collectionframework.ex;
+
+import java.util.*;
+
+public class Main {
+   public static void main(String[] str){
+        //Set
+        Set<String> setIter = new HashSet<>();
+        setIter.add("c");
+        setIter.add("f");
+        setIter.add("a");
+        setIter.add("b");
+
+        Iterator iterator = setIter.iterator();
+        while ((iterator.hasNext())) {
+            Object o = iterator.next();
+            System.out.print(o.toString());
+        }
+        
+        System.out.println("\n -----------");
+
+        iterator = setIter.iterator();
+        while ((iterator.hasNext())) {
+           Object o = iterator.next();
+            if(o.toString().equals("b")) iterator.remove();
+        }
+
+       iterator = setIter.iterator();
+       while ((iterator.hasNext())){
+           Object o = iterator.next();
+           System.out.print(o.toString());
+       }
+
+       System.out.println("\n ===========");
+
+       //List
+       List<String> list = new ArrayList<>();
+       list.add("c");
+       list.add("f");
+       list.add("a");
+       list.add("b");
+
+       System.out.println("\n -----------");
+
+       Iterator listIter = list.iterator();
+       while ((listIter.hasNext())){
+           Object o = listIter.next();
+           System.out.print(o.toString());
+       }
+    }
+}
---
+++ src/comparable_compare/ex/Main.java
@@ -0,0 +1,10 @@
+package comparable_compare.ex;
+
+import java.util.Set;
+
+public class Main {
+    static void main(String[] str){
+        
+
+    }
+}
